7a21fe8503759a23b7f84bfd813dd24a09e073e2
Jordan Gross <jgross18@me.com>
2020-11-12 10:12:24 -0600

    Initial commit

---
+++ database/migrations/2020_11_12_044413_create_test_transaction_v1-v50.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestTransactionV1V50 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
